0d8829b02dc5924cebddd243c09fbcacea895a94
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-05-23 15:34:35 +0400

    CommonResponse

---
+++ SCAA-CO/SCAA-API/CommonResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace SCAA_API
+{
+    public sealed class CommonResponse
+    {
+        public string Message { get; set; }
+        public object Result { get; set; }
+        public bool IsSuccess { get; set; }
+        public int HttpStatusCode { get; set; }
+
+        public CommonResponse(object result, string message, bool isSuccess, int httpStatusCode)
+        {
+            Message = message;
+            Result = result;
+            IsSuccess = isSuccess;
+            HttpStatusCode = httpStatusCode;
+        }
+
+        public CommonResponse()
+        {
+        }
+    }
+
+
+    public static class CommonResponseMessage
+    {
+        public static string SuccessMessage { get; } = "Request completed successfully";
+    };
+}
